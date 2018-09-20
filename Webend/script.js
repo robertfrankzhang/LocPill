874557d630e8d39331c8f57766197d433b5126bd
robertfrankzhang <robertzhang100@gmail.com>
2018-09-19 23:23:34 -0400

    Home Animations

--- Webend/script.js
+++ Webend/script.js
@@ -6,16 +6,28 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   console.log(scrolled)
-  if (scrolled <70){
+  if (scrolled <80){
     document.getElementById("top-pill").style.top = 300-(scrolled)/2 + "px";
     document.getElementById("bottom-pill").style.top = 455+(scrolled)/2 + "px";
     document.getElementById("home-main-panel-text-actual").style.opacity = (20-scrolled)/20;
-    $('#top-pill').removeClass('stick');
-    $('#bottom-pill').removeClass('stick');
+    document.getElementById("top-pill").style.opacity = 1;
+    document.getElementById("bottom-pill").style.opacity = 1;
   }else{
-    $('#top-pill').addClass('stick');
-    $('#bottom-pill').addClass('stick');
-    document.getElementById("top-pill").style.top = 300-(70)/2+700 + "px";
-    document.getElementById("bottom-pill").style.top = 455+(70)/2+700 + "px";
+    document.getElementById("top-pill").style.opacity = (90-scrolled)/10;
+    document.getElementById("bottom-pill").style.opacity = (90-scrolled)/10;
+    document.getElementById("base-label").style.opacity = (90-scrolled)/10;
+    document.getElementById("cap-label").style.opacity = (90-scrolled)/10;
+    document.getElementById("container-label").style.opacity = (90-scrolled)/10;
+
+  }
+
+  if (scrolled>15 && scrolled < 80){
+    document.getElementById("base-label").style.opacity = (scrolled-15)/10;
+    document.getElementById("cap-label").style.opacity = (scrolled-15)/10;
+    document.getElementById("container-label").style.opacity = (scrolled-15)/10;
+  }else if (scrolled<=15){
+    document.getElementById("base-label").style.opacity = 0;
+    document.getElementById("cap-label").style.opacity = 0;
+    document.getElementById("container-label").style.opacity = 0;
   }
 }
